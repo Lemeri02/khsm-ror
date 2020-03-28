500e6132d4fdc52b4f49106b21a940a77d8409ee
Lemeri02 <9588504@gmail.com>
2020-03-28 14:54:30 +0500

    test game_question, game, question models

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -11,6 +11,8 @@ require 'rails_helper'
 # авторов rails). Смысл именно в проверке _наличия_ у модели конкретных
 # валидаций.
 RSpec.describe Question, type: :model do
+  subject { Question.new(text: 'some', level: 0, answer1: '1', answer2: '1', answer3: '1', answer4: '1') }
+
   context 'validations check' do
     it { should validate_presence_of :text }
     it { should validate_presence_of :level }
@@ -19,5 +21,6 @@ RSpec.describe Question, type: :model do
 
     it { should allow_value(14).for(:level) }
     it { should_not allow_value(15).for(:level) }
+    it { should validate_uniqueness_of(:text) }
   end
 end
